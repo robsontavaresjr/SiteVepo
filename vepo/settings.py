7282a6ecc23bc8156bcc8439c778deff95dad536
Robson Tavares Júnior <robsont@me.com>
2018-09-26 13:30:22 -0300

    modificando alguns template taggins no código a fim de renderizar o .css

--- vepo/settings.py
+++ vepo/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-STATIC_URL = '/static/'
+STATIC_URL = '/webapp/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, STATIC_URL),
 )
